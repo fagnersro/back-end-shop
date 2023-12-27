d6c89314fdd43a06228b9c8e9d568754d6391f58
fagnersro <fagner.hg40@gmail.com>
2023-12-27 20:54:02 -0300

    login authentication route

--- routes/auth.js
+++ routes/auth.js
@@ -22,5 +22,25 @@ router.post("/register", async (req, res) => {
 });
 
 //LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username })
+    !user && res.status(401).json("Wrong credentials!")
+    
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password, 
+      process.env.PASS_SEC
+    );
+    const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    Originalpassword !== req.body.password && 
+      res.status(401).json("Wrong credentials!");
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router;
