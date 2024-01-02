791b1e4a1499ebcb6ad73133f43ca8ed742107f7
fagnersro <fagner.hg40@gmail.com>
2024-01-02 10:41:20 -0300

    admin token verification

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -17,11 +17,25 @@ const verifyToken = (req, res, next) => {
 const verifyTokenAndAuthorization = (req, res, next) => {
   verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
-      next()
+      next();
     } else {
       res.status(403).json("You are not alowed to do that!")
     }
-  })
-}
+  });
+};
 
-module.exports = { verifyToken, verifyTokenAndAuthorization  };
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!")
+    }
+  });
+};
+
+module.exports = { 
+  verifyToken, 
+  verifyTokenAndAuthorization,  
+  verifyTokenAndAdmin
+};
