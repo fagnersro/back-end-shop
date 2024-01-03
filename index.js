aefb5796bdb2a03179586de1fac6bc4a0e8981c0
fagnersro <fagner.hg40@gmail.com>
2024-01-03 13:08:06 -0300

    cart route methods

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRouter = require("./routes/cart");
 
 dotenv.config()
 
@@ -18,7 +19,8 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/cart", cartRouter);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!")
-})
+});
